5c87272466f1a2bdde8f64eaaeb995d0d7a0f027
Brooks Townsend <brooksmtownsend@gmail.com>
2021-06-06 11:24:05 -0400

    working firebase integration

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 require('dotenv').config()
-
+var admin = require('firebase-admin');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Firebase setup
+var serviceAccount = require(process.env.FIREBASE_ADMIN);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+// Discord ready
 client.once('ready', () => {
 	console.log('Deckard is ready to count deaths');
 });
 
+let getDeaths = () => {
+	db.collection('deathcount').get().then(snapshot => {
+		snapshot.forEach((doc) => {
+			console.log(doc.id)
+			console.log(process.env.DEATH_COLLECTION)
+			if (doc.id == process.env.DEATH_COLLECTION) {
+				return doc.data();
+			}
+		});
+	});
+}
+
+console.log(getDeaths());
+
+// Death counter
 client.on('message', message => {
+	if (!message.startsWith("!")) {
+		return
+	}
+
 	switch(message.content) {
 		case "!brooks":
-			message.channel.send(`Brooks has died! Total death count: ${}`);
+			message.channel.send(`Brooks has died! Total death count: ${count}`);
 			break;
 		case "!cody":
 			break;
@@ -27,4 +55,6 @@ client.on('message', message => {
 	}
 });
 
+
+// Discord login
 client.login(process.env.DISCORD_TOKEN);
